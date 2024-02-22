c199131690636a626d115e40655c831ef928f67d
CUBINHO\CSHARPV5 <alberto.nunes@phoenix-santos.com.br>
2024-02-21 21:57:37 -0300

    components data seed

---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo":"https://img.nsctotal.com.br/wp-content/uploads/2023/06/tres-versoes-de-homem-aranha-800x531.jpeg",
+    "title":"Novo filme do homem aranha",
+    "description":"O filme do homem aranha que será lançado em 2025, promete ser um dos melhores filmes do ano."
+  },
+  {
+    "id": "2",
+    "photo":"https://i.ytimg.com/vi/LZBlXkDvhh4/maxresdefault.jpg",
+    "title":"Homem-Aranha: atravéz do AranhaVerso.",
+    "description":"aaaa"
+  },
+  {
+    "id": "3",
+    "photo":"https://image.api.playstation.com/vulcan/img/rnd/202010/2621/H9v5o8vP6RKkQtR77LIGrGDE.png",
+    "title":"Mergulhe no Universo do Cavaleiro das Trevas.",
+    "description":"aaaa"
+  },
+  {
+    "id": "4",
+    "photo":"https://static.itapemafm.com.br/s3fs-public/2020-06/henry%20cavill%20superman.jpg?yGfKzA6R_1p60WqurxiML9lDV_ZYe__y",
+    "title":"Henry Cavil pode viver Superman novamente.",
+    "description":"aaaa"
+  }
+];
